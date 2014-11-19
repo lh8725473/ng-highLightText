b74f6e04e8359dba123f36be7dfe408823b18d85
lh8725473 <171213574@qq.com>
2014-11-19 16:12:03 +0800

    timeout

--- dome/js/ng-highLightText.js
+++ dome/js/ng-highLightText.js
@@ -1,9 +1,11 @@
-angular.module('ngHighLightText', []).directive('highLightText', [
-  function() {
+angular.module('ngHighLightText', []).directive('highLightText', ['$timeout',
+  function($timeout) {
     return {
       restrict: 'A',
       link: function(scope, element, attrs) {
-        element.highLightText(scope[attrs.highLightText].text, scope[attrs.highLightText])
+        $timeout(function(){
+          element.highLightText(scope[attrs.highLightText].text, scope[attrs.highLightText])
+        })
       }
     }
   }
